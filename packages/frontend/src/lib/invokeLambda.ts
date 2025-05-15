b3407e2b2dc694372d25e31d2990a8d422bd3215
MohdAliNasser <mohamed.ali@iga.gov.bh>
2025-05-15 08:51:16 +0300

    Update invokeLambda.ts
    Orginal file

--- packages/frontend/src/lib/invokeLambda.ts
+++ packages/frontend/src/lib/invokeLambda.ts
@@ -1,50 +1,72 @@
+import { AwsClient } from "aws4fetch";
+
 import { getCurrentUser } from "./getToken.ts";
 import { getUserToken } from "./getToken.ts";
+import getAwsCredentials from "./getIAMCred.ts";
 
 export default async function invokeLambda({
   method = "GET",
   body,
-  path,
-  
+  url,
 }: {
   method?: string;
   body: any;
-  path: string;
-  
+  url: string;
 }) {
   const currentUser = getCurrentUser();
   if (!currentUser) {
     throw new Error("User is not authenticated");
   }
 
-  const token = await getUserToken(currentUser);
+  const userToken = await getUserToken(currentUser); // User token from the Cognito user pool
+
+  // Retrieve IAM credentials based on the Cognito token
+  //@ts-ignore
+  const credentials = await getAwsCredentials(userToken);
+
+  //@ts-ignore
+  const { accessKeyId, secretAccessKey, sessionToken } = credentials;
+
+
+  if (!accessKeyId || !secretAccessKey || !sessionToken) {
+    throw new Error("AWS credentials are not available.");
+  }
+
+  //   const signedFetch = createSignedFetcher({
+  //     service: "lambda", // depends on what you want to access
+  //     region: import.meta.env.VITE_REGION,
+  //     credentials: {
+  //       accessKeyId: accessKeyId,
+  //       secretAccessKey: secretAccessKey,
+  //       sessionToken: sessionToken,
+  //     },
+  //   });
+
+  const aws = new AwsClient({
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+      sessionToken: sessionToken,
+    service: "lambda"
+  });
+
+  body = body ? JSON.stringify(body) : body;
 
   try {
-    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}${path}`, {
-      method,
+    const response = await aws.fetch(url, {
+      method: method,
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
       },
-      body: body ? JSON.stringify(body) : undefined,
+      body: body,
     });
 
     if (!response.ok) {
       const errorText = await response.text();
-      console.error("❌ Error response body:", errorText);
-
-      let parsedError;
-      try {
-        parsedError = JSON.parse(errorText);
-      } catch (e) {
-        throw new Error(`API returned invalid response: ${errorText}`);
-      }
-
-      throw new Error(
-        parsedError.details || parsedError.error || "API call failed"
-      );
+      console.error("Error response body:", errorText);
+      throw new Error(`API call failed: ${errorText}`);
     }
 
+    
     return response;
   } catch (error) {
     throw new Error(
