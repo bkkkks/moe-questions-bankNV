8768a3b4ea1d079185e10a15912e8360129b5889
MohdAliNasser <mohamed.ali@iga.gov.bh>
2025-05-25 07:57:26 +0300

    Update ViewExam.tsx
    to contributors

--- packages/frontend/src/pages/ViewExam.tsx
+++ packages/frontend/src/pages/ViewExam.tsx
@@ -207,7 +207,7 @@ const ViewExam: React.FC = () => {
       setSemester(response.examSemester || "");
       setCreator(response.createdBy || "");
       setDate(response.creationDate || "");
-      setContributers(String(response.contributors || ""));
+      setContributors(String(response.contributors || "")); // edited to contributors
       setDuration(response.examDuration || "");
       setMark(response.examMark || "");
       setExamState(response.examState || "");
