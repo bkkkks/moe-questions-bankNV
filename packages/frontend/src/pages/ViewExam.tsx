074ceccbc42ef2f117e87f8a13215ec7b7ab447d
MohdAliNasser <mohamed.ali@iga.gov.bh>
2025-05-28 11:28:43 +0300

    Update ViewExam.tsx

--- packages/frontend/src/pages/ViewExam.tsx
+++ packages/frontend/src/pages/ViewExam.tsx
@@ -90,14 +90,17 @@ const ViewExam: React.FC = () => {
     try {
       setLoadingChangeState(true);
 
-      const functionURL = import.meta.env.VITE_CREATE_EXAM_FUNCTION_URL;
-      console.log("Function URL:", functionURL);
+     // const functionURL = import.meta.env.VITE_CREATE_EXAM_FUNCTION_URL;
+      //console.log("Function URL:", functionURL);
+
+      const apiURL = `${import.meta.env.VITE_API_URL}/createNewExam`;
 
       const response = await invokeLambda({
         method: "POST",
         body: requestBody,
-        url: functionURL,
+        url: apiURL,
       });
+
 
       const data = await response.json();
 
