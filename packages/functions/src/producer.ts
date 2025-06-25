b71c174394ef61b95b2e89ab5267286cb54b1f91
MohdAliNasser <mohamed.ali@iga.gov.bh>
2025-06-25 14:48:40 +0300

    Create producer.ts

---
+++ packages/functions/src/producer.ts
@@ -0,0 +1,20 @@
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+
+const sqs = new SQSClient({});
+const QUEUE_URL = process.env.QUEUE_URL!;
+
+export async function handler(event: any) {
+  const body = JSON.parse(event.body);
+
+  const command = new SendMessageCommand({
+    QueueUrl: QUEUE_URL,
+    MessageBody: JSON.stringify(body),
+  });
+
+  await sqs.send(command);
+
+  return {
+    statusCode: 202,
+    body: JSON.stringify({ message: "Exam creation request queued." }),
+  };
+}
