dbff70403145c1699fc452960018050436808e8b
MohdAliNasser <mohamed.ali@iga.gov.bh>
2025-07-10 09:47:55 +0300

    Update ExamForm.tsx

--- packages/frontend/src/pages/ExamForm.tsx
+++ packages/frontend/src/pages/ExamForm.tsx
@@ -1,6 +1,7 @@
 
 
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import invokeApig from "../lib/callAPI.ts";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
