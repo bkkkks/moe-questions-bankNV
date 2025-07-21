aad170ff43e988f50691a299d1d4969f6506fc12
MohdAliNasser <mohamed.ali@iga.gov.bh>
2025-07-21 13:51:23 +0300

    Update ExamForm.tsx

--- packages/frontend/src/pages/ExamForm.tsx
+++ packages/frontend/src/pages/ExamForm.tsx
@@ -72,7 +72,7 @@ const ExamForm: React.FC = () => {
 
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
-  const { showAlert } = useAlert();
+  const { showAlert, hideAlert } = useAlert();
 
   
   let pollAttempts = 0;
