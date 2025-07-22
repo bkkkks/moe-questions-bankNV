c3580e78bd050d22c0bcb592d5fe6830a9cfa819
MohdAliNasser <mohamed.ali@iga.gov.bh>
2025-07-22 10:38:14 +0300

    Update ExamForm.tsx

--- packages/frontend/src/pages/ExamForm.tsx
+++ packages/frontend/src/pages/ExamForm.tsx
@@ -189,11 +189,11 @@ const ExamForm: React.FC = () => {
 
       console.log("Raw Exam Content from Backend:", response.examContent);
 
-      if (!response.examContent) {
+     /* if (!response.examContent) {
         showAlert({
           type: "failure",
           message: "Failed to load",
-        });
+        });*/
         return;
       }
 
